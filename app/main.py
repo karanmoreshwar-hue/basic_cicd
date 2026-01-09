10ca284dcfa47af8de5da0b56fc27f1d941d3306
karanmoreshwar-hue <karan.moreshwar@sigmoidanalytics.com>
2026-01-09 16:36:42 +0530

    Initial commit with CI/CD pipeline

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "Application is running"}
+
+@app.get("/health")
+def health():
+    return {"health": "broken"}
+
